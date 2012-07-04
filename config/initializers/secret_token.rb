c957e16deee6437ce6184f976d7435cd6cd41cf3
Trey Lawrence <lawrence.trey@gmail.com>
2012-07-04 14:34:59 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Newbeer4me::Application.config.secret_token = '5a0dc767a95ab811db41bac81d92d27ba35dcdd2fce3fa8fb6b082af3d6fdb16fe8b6b21ee573aaf7396b5b859b3868c10cfd735852596b91dc0cac45288dfee'
