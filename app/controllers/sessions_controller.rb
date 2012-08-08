0759744703ffc61b31bcc01c7decc0bf33aa5fc5
Trey Lawrence <lawrence.trey@gmail.com>
2012-08-07 22:23:06 -0400

    Remove logging and redirect to root

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,16 +3,14 @@ class SessionsController < ApplicationController
   end
   
   def create
-    logger.debug "PARAMSBICHES!!!!!!!!!#{params}"
     if !(params[:untappd_info][:username].empty? && params[:untappd_info][:password].empty?)
       current_user.untappd_username = params[:untappd_info][:username]
       current_user.password = params[:untappd_info][:password]
       current_user.last_seen_at = Time.now
       if sign_in_to_untappd
-        logger.debug "Successfully signed into untappd, save user and redirect to root path"
         session[:name] = current_user.untappd_username
         current_user.save
-        render 'beers/index'
+        redirect_to root_path
         return
       else
         logger.debug "Unable to sign user in"
