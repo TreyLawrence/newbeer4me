07469422f4e283f4bb437c7f83e3bdd0b4fb7c34
Trey Lawrence <lawrence.trey@gmail.com>
2012-08-07 22:21:05 -0400

    Change spell check naming

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -8,16 +8,16 @@ class BeersController < ApplicationController
     if params[:search]
       if params[:type] == "beer"
         beer = Beer.new(params[:search].strip, params[:id], browser)
-        beer.spell_check
+        beer.spell_check_beer
         if beer.search_untappd
           render text: {result: render_to_string(beer), id: params[:id], type: params[:type]}.to_json
         else
           render text: {result: "Error logging in", id: params[:id], type: params[:type]}.to_json
         end
-      else
+      elsif params[:type] == "venue"
         venue = Venue.new(params[:search].strip, params[:id], browser)
-        venue.spell_check
-        venue.search_beer_menus
+        venue.spell_check_venue
+        venue.search_untappd
         render text: {result: render_to_string(venue), id: params[:id], type: params[:type]}.to_json
       end
     else
@@ -55,30 +55,31 @@ class BeersController < ApplicationController
   def checkin
     render nothing: true
     current_user = User.find_by_foursquare_id(JSON.parse(params[:checkin])['user']['id'])
-    logger.info "Current user is #{current_user}"
+    logger.info "Current user is #{current_user.untappd_username}"
     shout = JSON.parse(params[:checkin])['shout']
     logger.info ("Shout is #{JSON.parse(params[:checkin])['shout']}")
     if current_user && (shout =~ /beer/i)
       logger.info "Venue name is #{JSON.parse(params[:checkin])['venue']['name'] ? JSON.parse(params[:checkin])['venue']['name'] : 'nil'}"
       venue = Venue.new(JSON.parse(params[:checkin])['venue']['name'], 1, browser)
-      venue.search_beer_menus
+      venue.search_utappd
       if venue.beers.count > 0
         venue.beers.each {|beer| logger.info "beers #{beer.name}" }
+        
         new_beer_names = 'Try these: ' + venue.beers.select { |beer| !beer.had}.map { |new_beer| new_beer.name}.join(' ')
       else
         new_beer_names = "This bar isn't on beermenus, bro :("
       end
       
       url = 'https://api.foursquare.com/v2/checkins/' + 
-            "#{JSON.parse(params[:checkin])['id']}/reply" + 
+            "#{current_user.foursquare_id}/reply" + 
             "?oauth_token=#{current_user.foursquare_token}" +
             "&text=#{new_beer_names.gsub(' ', '%20')[0..160]}"
 
-      logger.info url
+      logger.info "url: #{url}"
       
       begin
         page = browser.post('https://api.foursquare.com/v2/checkins/' + 
-                            "#{JSON.parse(params[:checkin])['id']}/reply" + 
+                            "#{current_user.foursquare_id}/reply" + 
                             "?oauth_token=#{current_user.foursquare_token}" +
                             "&text=#{new_beer_names.gsub(' ', '%20')[0..160]}")
       rescue Mechanize::Error => e
