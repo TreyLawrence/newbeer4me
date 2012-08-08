438c49cc847640591600fcd26c41999dbadd5e3e
Trey Lawrence <lawrence.trey@gmail.com>
2012-08-07 22:29:06 -0400

    Lazily initialize spell check, and allow initialization with Mechanize::Page instead of string

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,46 +1,49 @@
 class Beer
   include ActiveModel::Validations
-  attr_accessor :search, :id, :spell_check, :name, :brewery, :had, :debug_string
+  attr_accessor :search, :id, :spell_check, :name, :brewery, :had, :debug_string, :avg_rating, :my_rating
   validates :search, presence: true
   
-  def initialize beer_name, beer_id, mechanize_object
+  def initialize beer, beer_id, mechanize_object
+    if beer.class == String
+      @search = beer
+    elsif beer.class == Mechanize::Page
+      @beer_page = beer
+    else
+      raise "Need beer as String or Mechanize::Page"
+    end
+
     @browser = mechanize_object
-    @search = beer_name
     @id = beer_id
     @debug_string = ""
   end
   
   def spell_check
-    doc = Nokogiri::HTML(@browser.get("http://www.google.com/search?q=#{@search + ' beer'}").body)
-    correction_link = doc.css('a').select { |link| link['href'].include? "spell=1" }.first
-    @spell_check = correction_link.text.split[0..-2].join(' ') if correction_link
+    if @spell_check.nil?
+      search_result = @browser.get("http://www.google.com/search?q=#{@search + ' beer'}")
+      correction_link = search_result.links.select { |link| link.uri.to_s =~ /spell=1/ }.first
+      @spell_check = correction_link.to_s.split[0..-2].join(' ') if correction_link
+    else
+      @spell_check
+    end
   end
-  
-  def search_untappd
-    query = @spell_check || @search
-    
-    url = "http://untappd.com/search?q=#{query}"
-    debug_string << "Searching untappd: #{url}\n"
 
-    page = @browser.get("http://untappd.com/search?q=#{query}")
+  def search_untappd
+    if @search.nil?
+      beer_page = @beer_page
+    else
+      query = @spell_check || @search
+      query_page = @browser.get("http://untappd.com/search?q=#{query}")
+      beer_links = query_page.links.select { |link| link.uri.to_s[/beer\/\d+/] rescue nil }
+      beer_page = beer_links.first.click unless beer_links.empty?
+    end
     
-    if page.links.select { |link| link.uri.to_s[/login/] rescue nil}.empty?
-      debug_string << "Signed in to untappd\n"
-      beer_links = page.links.select { |link| link.uri.to_s[/beer\/\d+/] rescue nil }
-      link = beer_links.first
-      if link
-        doc = Nokogiri::HTML(link.click.body)
-        @brewery = doc.css('a').select {|link| link['href'].include? "brewery" }.first.text
-        @name = link.text
-        if doc.css('.drank.tip').to_s.empty?
-          @had = false
-        else
-          @had = true
-        end
-      end
+    if beer_page.links.select { |link| link.uri.to_s[/login/] rescue nil}.empty?
+      @brewery = beer_page.search("ul.beer-details").css('a').text
+      @name = beer_page.search("ul.beer-details").css('h2').text
+      @had = !beer_page.search('.drank.tip').empty?
+      @avg_rating = beer_page.search('ul.rating-display').css('span')[1].text.strip
       true
     else
-      debug_string << "Not signed in to untappd\n"
       false
     end
   end
