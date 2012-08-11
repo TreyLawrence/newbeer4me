2a24e45cb95192aae2cd9ad759898f3f60b51b75
Trey Lawrence <lawrence.trey@gmail.com>
2012-08-11 15:14:48 -0400

    Cleaned up javascript file, and added timeout support for a search.

--- app/assets/javascripts/beers.js
+++ app/assets/javascripts/beers.js
@@ -1,10 +1,15 @@
-(function() {
-var ENTER = 13;
-var search_count = {'beer' : 0, 'venue' : 0}
-var collapsible_left = '<div data-role="collapsible" data-content-theme="c"><h3>';
-var collapsible_right = "<img alt='Busy' src='/assets/busy.gif' align='right' ></h3><p>...</p></div>";
-var search_type = 'beer';
 $(document).ready(function() {
+var ENTER = 13;
+
+var search_count = {'beer' : 0, 'venue' : 0}
+var search_type = 'beer';
+
+var collapsible_left_busy = '<div data-role="collapsible" data-content-theme="c"><h3>';
+var collapsible_right_busy = "<img alt='Busy' src='/assets/busy.gif' align='right' ></h3><p>...</p></div>";
+var collapsible_left_timeout = '<div data-role="collapsible" data-content-theme="c"><h3>Search timed out</h3><p>Searched for ';
+var collapsible_right_timeout = '</p></div>'
+
+$(function() {
 	$("#search").keypress(function(event) {
 		if ( event.which == ENTER ) {
 			event.preventDefault();
@@ -12,67 +17,44 @@ $(document).ready(function() {
 			if ( text ) {
 				search_count[search_type] += 1;
 				$('#search').val('');
-				$(collapsible_left + text + collapsible_right).addClass(search_type+search_count[search_type]).prependTo('[data-role="collapsible-set"].'+search_type+'s');
+				$(collapsible_left_busy + text + collapsible_right_busy)
+						.addClass(search_type+search_count[search_type])
+						.prependTo('[data-role="collapsible-set"].'+search_type+'s');
 				$('[data-role="collapsible-set"].'+search_type+'s').collapsibleset('refresh');
-				$.ajaxSetup({
-					beforeSend: function(xhr) {
-						xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'));
-					}
-				});
-				$.ajax({
-					type: "POST",
-					url: "/search",
-					data: {search: text, id: search_count[search_type], type: search_type}
-				}).done(function( msg ) {
-					var obj = jQuery.parseJSON(msg);
-					$('[data-role="collapsible"].'+obj.type+obj.id).replaceWith(obj.result);
-					$('[data-role="collapsible-set"].'+obj.type+'s').collapsibleset('refresh');
-					if(obj.type == "venue") {
-						$('[data-role="collapsible-set"].venue'+obj.id+'_beers_had').trigger('create').collapsibleset().collapsibleset('refresh');
-						$('[data-role="collapsible-set"].venue'+obj.id+'_beers_nothad').trigger('create').collapsibleset().collapsibleset('refresh');
-					}
-				});
+				ajax_send(text, search_count[search_type], search_type);
 			}
 		}
 	});
 });
 
-$(document).ready(function() {
-	$('.ui-slider-label-a').change(function() {
-		//if ($(this).val() == "on")
-		//{
-			alert("changed!");
-		//} else {
-		//	alert("off!");
-			/*$.ajaxSetup({
-				beforeSend: function(xhr) {
-					xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'));
-				}
-			});
-			$.ajax({
-				type: "POST",
-				url: "/disable",
-			});*/
-		//}
-	});
-});
-
 $(function() {
+	$('a[data-icon="refresh"]').live("click", function(){
+		var name = $(this).attr("search_name");
+		var id = $(this).attr("search_id");
+		var type = $(this).attr("search_type");
+
+		var busy_collapsible = $(collapsible_left_busy + name + collapsible_right_busy)
+				.addClass(type+id);
+		$('[data-role="collapsible"].'+type+id).replaceWith(busy_collapsible);
+		$('[data-role="collapsible-set"].'+type+'s').collapsibleset('refresh');
+
+		ajax_send(name, id, type);
+	});
     $('a[title="beer"]').click(function(){
-		$('#search').attr("placeholder", "Beer...");
 		$(this).addClass("ui-btn-active");
 		$('a[title="venue"]').removeClass("ui-btn-active");
+		$('#search').attr("placeholder", "Beer...");
 		$('[data-role="collapsible-set"].venues').hide();
 		$('[data-role="collapsible-set"].beers').show();
 		search_type = 'beer';
 	});
 	$('a[title="venue"]').click(function(){
-		$('#search').attr("placeholder", "Venue...");
 		$(this).addClass("ui-btn-active");
 		$('a[title="beer"]').removeClass("ui-btn-active");
-		search_type = 'venue';
+		$('#search').attr("placeholder", "Venue...");
 		$('[data-role="collapsible-set"].beers').hide();
 		$('[data-role="collapsible-set"].venues').show();
+		search_type = 'venue';
     });
 	$('a.foursquare').click(function(){
 		$(this).replaceWith($(document.createElement("img")).attr("src","/assets/foursquare-busy.gif"));
@@ -83,4 +65,71 @@ $(function() {
 	});
 });
 
-}).call(this);
+
+function ajax_send(text, id_num, search_type){
+	if(search_type == "beer") {
+		var timeout_time = 15000; //15 seconds for beers
+	} else {
+		var timeout_time = 60000; //60 seconds for venues
+	}
+	
+	$.ajaxSetup({
+		beforeSend: function(xhr) {
+			xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'));
+		}
+	});
+	$.ajax({
+		type: "POST",
+		url: "/search",
+		dataType: "json",
+		timeout: timeout_time,
+		data: {search: text, id: id_num, type: search_type}
+	}).done(function( msg ) {
+		$('[data-role="collapsible"].'+msg.type+msg.id).replaceWith(msg.result);
+		$('[data-role="collapsible-set"].'+msg.type+'s').collapsibleset('refresh');
+		if(msg.type == "venue") {
+			$('[data-role="collapsible-set"].venue'+msg.id+'_beers_had')
+					.trigger('create')
+					.collapsibleset()
+					.collapsibleset('refresh');
+			$('[data-role="collapsible-set"].venue'+msg.id+'_beers_nothad')
+					.trigger('create')
+					.collapsibleset()
+					.collapsibleset('refresh');
+		}
+	}).error(function(jqXHR, textStatus, errorThrown) {
+		if(textStatus==="timeout") {
+            var data = params_unserialize(this.data);
+			var button = $("<a>").attr("href", "#")
+		            .attr("data-role", "button")
+		            .attr("data-icon", "refresh")
+		 			.attr("data-iconpos", "notext")
+					.attr("search_type", data.type)
+					.attr("search_id", data.id)
+					.attr("search_name", data.search)
+					.buttonMarkup({ inline: true })
+					.button();
+			var new_collapsible = $(collapsible_left_timeout + data.search + collapsible_right_timeout)
+					.addClass(data.type+data.id);
+					
+			button.appendTo(new_collapsible.find('p'));
+
+			$('[data-role="collapsible"].'+data.type+data.id).replaceWith(new_collapsible);
+			$('[data-role="collapsible-set"].'+data.type+'s').collapsibleset('refresh');
+        }
+    });
+}
+
+function params_unserialize(p){
+	var ret = {},
+    seg = p.replace(/^\?/,'').replace(/\+/,' ').split('&'),
+    len = seg.length, i = 0, s;
+	for (;i<len;i++) {
+	    if (!seg[i]) { continue; }
+	    s = seg[i].split('=');
+	    ret[s[0]] = s[1];
+	}
+	return ret;
+}
+
+});
