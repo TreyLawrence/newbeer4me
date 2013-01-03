9ba2a5ec99293d5be57560ff3d7b59d50c08eee8
Trey Lawrence <lawrence.trey@gmail.com>
2013-01-03 01:19:53 -0500

    Replace with preloaded image

--- app/assets/javascripts/beers.js
+++ app/assets/javascripts/beers.js
@@ -57,7 +57,7 @@ $(function() {
 		search_type = 'venue';
     });
 	$('a.foursquare').live("click", function(){
-		$(this).replaceWith($(document.createElement("img")).attr("src","/assets/busy-bar.gif"));
+		$(this).replaceWith(busyBar);
 		window.location = "https://foursquare.com/oauth2/authenticate" +
                     "?client_id=DLTI5SNFPYNXCV4AKZYYPZWXOFWLS3B2ZBGOWEC1DB1NO3BJ" +
                     "&response_type=code" +
