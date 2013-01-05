0a08060d8956d061514d13bb4cc6b47b1b812c15
Trey Lawrence <lawrence.trey@gmail.com>
2013-01-05 16:53:20 -0500

    Create javascript file for signing in

---
+++ app/assets/javascripts/sessions.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	$('a.untappd').live("click", function(){
+		$(this).replaceWith(busyBar);
+		/*window.location = "http://untappd.com/oauth/authenticate/" +
+					"?client_id=9D467E2DB6750B919F95D156BA2099E32EF909E3" +
+					"&client_secret=2EE43793B625BA1BD4E19E2111DF4E5C99045CAD" +
+					"&response_type=code" +
+					"&redirect_url=https://newbeer4me.herokuapp.com/untappd";*/
+	});
+});
