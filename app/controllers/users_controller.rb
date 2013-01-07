b6a8eb1c8e9773d74b1a1a31891921db91bc841e
Trey Lawrence <lawrence.trey@gmail.com>
2013-01-07 01:32:06 -0500

    Add untappd oauth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,21 +26,22 @@ class UsersController < ApplicationController
       current_user.save
     elsif params[:code]
       begin
-        oauth_json = browser.get("http://untappd.com/oauth/authorize/" +
+        oauth_json = JSON.parse(browser.get("http://untappd.com/oauth/authorize/" +
                            "?client_id=#{untappd_client_id}" +
                            "&client_secret=#{untappd_client_secret}" +
                            "&response_type=code" +
                            "&redirect_url=https://newbeer4me.herokuapp.com/connect/untappd" +
-                           "&code=#{params[:code]}")
+                           "&code=#{params[:code]}").body)
 
-        current_user.untappd_token = JSON.parse(oauth_json.body)['access_token']
+        current_user.untappd_token = oauth_json['access_token']
 
-        page = browser.get('http://untappd.com/v4/user/info' +
-                          "?access_token=#{current_user.untappd_token}")
+        user_json = JSON.parse(browser.get('http://api.untappd.com/v4/user/info' +
+                          "?access_token=#{current_user.untappd_token}").body)
 
-        current_user.untappd_id = JSON.parse(page.body)["response"]["user"]["id"]
+        current_user.first_name |= user_json["response"]["user"]["first_name"]
+        current_user.last_name |= user_json["response"]["user"]["last_name"]
 
-        if current_user.foursquare_id && current_user.foursquare_token
+        if current_user.untappd_token
           current_user.save
         end
       rescue Mechanize::Error => e
@@ -51,7 +52,6 @@ class UsersController < ApplicationController
 
   def disable_untappd
     current_user.untappd_token = nil
-    current_user.untappd_id = nil
     current_user.save
     redirect_to settings_path
   end
@@ -63,19 +63,19 @@ class UsersController < ApplicationController
       current_user.save
     elsif params[:code]
       begin
-        page = browser.get('https://foursquare.com/oauth2/access_token' +
+        oauth_json = JSON.parse(browser.get('https://foursquare.com/oauth2/access_token' +
                           "?client_id=#{foursquare_client_id}" +
                           "&client_secret=#{foursquare_client_secret}" +
                           "&grant_type=authorization_code" +
                           '&redirect_uri=https://newbeer4me.herokuapp.com/connect/foursquare' +
-                          "&code=#{params['code']}")
+                          "&code=#{params['code']}").body)
 
-        current_user.foursquare_token = JSON.parse(page.body)['access_token']
+        current_user.foursquare_token = oauth_json['access_token']
 
-        page = browser.get('https://api.foursquare.com/v2/users/self' +
-                          "?oauth_token=#{current_user.foursquare_token}")
+        user_json = JSON.parse(browser.get('https://api.foursquare.com/v2/users/self' +
+                          "?oauth_token=#{current_user.foursquare_token}").body)
 
-        current_user.foursquare_id = JSON.parse(page.body)["response"]["user"]["id"]
+        current_user.foursquare_id = user_json["response"]["user"]["id"]
 
         if current_user.foursquare_id && current_user.foursquare_token
           current_user.save
