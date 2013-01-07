f944b90738c8298bbaa2af4b8f7377826be40747
Trey Lawrence <lawrence.trey@gmail.com>
2013-01-07 01:54:29 -0500

    Match redirect url

--- app/assets/javascripts/sessions.js
+++ app/assets/javascripts/sessions.js
@@ -5,6 +5,6 @@ $(document).ready(function() {
 					"?client_id=9D467E2DB6750B919F95D156BA2099E32EF909E3" +
 					"&client_secret=2EE43793B625BA1BD4E19E2111DF4E5C99045CAD" +
 					"&response_type=code" +
-					"&redirect_url=https://newbeer4me.herokuapp.com/untappd";
+					"&redirect_url=https://newbeer4me.herokuapp.com/connect/untappd";
 	});
 });
