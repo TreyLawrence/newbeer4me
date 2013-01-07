5d06e6f389ff6b5e9b155cffc67471b36defdabc
Trey Lawrence <lawrence.trey@gmail.com>
2013-01-07 02:28:23 -0500

    Create new user if not signed in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,34 +20,35 @@ class UsersController < ApplicationController
   end
 
   def connect_untappd
-    if params[:error]
-      current_user.foursquare_token = nil
-      current_user.foursquare_id = nil
-      current_user.save
-    elsif params[:code]
+    if params[:code]
       begin
         oauth_json = JSON.parse(browser.get("http://untappd.com/oauth/authorize/" +
-                           "?client_id=#{untappd_client_id}" +
-                           "&client_secret=#{untappd_client_secret}" +
-                           "&response_type=code" +
-                           "&redirect_url=https://newbeer4me.herokuapp.com/connect/untappd" +
-                           "&code=#{params[:code]}").body)
-
-        current_user.untappd_token = oauth_json['access_token']
-
-        user_json = JSON.parse(browser.get('http://api.untappd.com/v4/user/info' +
+                          "?client_id=#{untappd_client_id}" +
+                          "&client_secret=#{untappd_client_secret}" +
+                          "&response_type=code" +
+                          "&redirect_url=https://newbeer4me.herokuapp.com/connect/untappd" +
+                          "&code=#{params[:code]}").body)
+                          
+        logger.info oauth_json
+        
+        current_user = User.new if !signed_in?
+        current_user.untappd_token = oauth_json['response']['access_token']
+        
+        logger.info "access_token #{oauth_json['response']['access_token']}"
+        logger.info "current_user #{current_user}"
+        
+        user_json = JSON.parse(browser.get("http://api.untappd.com/v4/user/info" +
                           "?access_token=#{current_user.untappd_token}").body)
-
+        
         current_user.first_name |= user_json["response"]["user"]["first_name"]
         current_user.last_name |= user_json["response"]["user"]["last_name"]
-
-        if current_user.untappd_token
-          current_user.save
-        end
+        
+        current_user.save if current_user.untappd_token
       rescue Mechanize::Error => e
         logger.info e
       end
     end
+    redirect_to root_path
   end
 
   def disable_untappd
@@ -57,19 +58,22 @@ class UsersController < ApplicationController
   end
 
   def connect_foursquare
-    if params[:error]
-      current_user.foursquare_token = nil
-      current_user.foursquare_id = nil
-      current_user.save
-    elsif params[:code]
+    if params[:code]
       begin
+        logger.info 'https://foursquare.com/oauth2/access_token' +
+                          "?client_id=#{foursquare_client_id}" +
+                          "&client_secret=#{foursquare_client_secret}" +
+                          "&grant_type=authorization_code" +
+                          '&redirect_uri=https://newbeer4me.herokuapp.com/connect/foursquare' +
+                          "&code=#{params['code']}"
         oauth_json = JSON.parse(browser.get('https://foursquare.com/oauth2/access_token' +
                           "?client_id=#{foursquare_client_id}" +
                           "&client_secret=#{foursquare_client_secret}" +
                           "&grant_type=authorization_code" +
                           '&redirect_uri=https://newbeer4me.herokuapp.com/connect/foursquare' +
                           "&code=#{params['code']}").body)
-
+                          
+        current_user = User.new if !signed_in?
         current_user.foursquare_token = oauth_json['access_token']
 
         user_json = JSON.parse(browser.get('https://api.foursquare.com/v2/users/self' +
@@ -84,6 +88,7 @@ class UsersController < ApplicationController
         logger.info e
       end
     end
+    redirect_to root_path
   end
 
   def disable_foursquare
@@ -96,11 +101,11 @@ class UsersController < ApplicationController
   private
 
     def untappd_client_secret
-      ENV['UNT_CLIENT_SECRET']
+      ENV['UNTAPPD_CLIENT_SECRET']
     end
 
     def untappd_client_id
-      ENV['UNT_CLIENT_ID']
+      ENV['UNTAPPD_CLIENT_ID']
     end
 
     def foursquare_client_secret
